75e8026c34237958c337a18268027b8be347c05b
Aude11 <vernet.aude@gmail.com>
2022-04-25 20:39:56 +0100

    add join mathods

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -4,8 +4,9 @@ import sys
 
 class DatabaseManager(): 
 
-    def __init__(self, connection):
+    def __init__(self, connection, playlist_length_limit):
         self.connection = connection
+        self.playlist_length_limit = playlist_length_limit
         #self.cur = self.connection.cursor()
 
     def read_all_table_items(self, table_name):
@@ -13,8 +14,16 @@ class DatabaseManager():
             cur = self.connection.cursor()
             cur.execute("SELECT * FROM %s" % table_name)
             result = cur.fetchall()
+            cur.close()
             for row in result:
                 print(row)
+
+    def join_table(self):
+        query = " SELECT  m.PK_SongID, m.Title from master_song_list m \
+                INNER JOIN key_words k on m.PK_SongID=k.FK_SongID \
+                where k.vibeID  = "IC" and k.SubjectID = "EX" \
+                ORDER BY RAND() LIMIT 8; "
+
 
     def __ensure_table_exist(self, table_name):
         cur = self.connection.cursor()
@@ -22,6 +31,7 @@ class DatabaseManager():
         tables = []
         for table in cur:
             tables.append(table[0])
+        cur.close()
         if table_name in tables:
             return True
         else:
