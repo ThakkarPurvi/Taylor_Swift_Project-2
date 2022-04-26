c31d7deb983d1f257e83065c5406999942a63da8
Aude11 <vernet.aude@gmail.com>
2022-04-26 14:01:18 +0100

    update config file for sportify api

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 HOST = "localhost"
 USER = "root"
-PASSWORD = "V1vaafrica"
+PASSWORD = ""
 
 CLIENT_ID = "348b2144fa4f49b0a31fd2db53d0f27b"
 CLIENT_SECRET = "2aec9aab8e474251827aa7939ae7dffd"
-AUTH_URL = 'https://accounts.spotify.com/api/token'
