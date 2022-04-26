66959343188ad69fab203b72fcd9ee09c3a36ded
Aude11 <vernet.aude@gmail.com>
2022-04-26 01:13:51 +0100

    add log for using spotify api

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 HOST = "localhost"
 USER = "root"
-PASSWORD = ""
+PASSWORD = "V1vaafrica"
+
+CLIENT_ID = "348b2144fa4f49b0a31fd2db53d0f27b"
+CLIENT_SECRET = "2aec9aab8e474251827aa7939ae7dffd"
+AUTH_URL = 'https://accounts.spotify.com/api/token'
