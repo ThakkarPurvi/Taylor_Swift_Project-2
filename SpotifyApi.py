f7bb55b9d43dd5001294a73db2294ebbbcd03d49
Aude11 <vernet.aude@gmail.com>
2022-04-26 01:13:25 +0100

    draft class spotify and api use

--- SpotifyApi.py
+++ SpotifyApi.py
@@ -1,12 +1,64 @@
 import requests
+from config import CLIENT_ID, CLIENT_SECRET, AUTH_URL 
 
-spotify_token = ""
-song_name = ""
-artist = ""
+
+class Spotify():
+    def __init__(self):
+        pass
+
+    def __get_token(self):
+        auth_response = requests.post(AUTH_URL, {
+                                                'grant_type': 'client_credentials',
+                                                'client_id': CLIENT_ID,
+                                                'client_secret': CLIENT_SECRET,
+                                                })
+
+        # convert the response to JSON
+        auth_response_data = auth_response.json()
+        # save the access token
+        spotify_token = auth_response_data['access_token']
+        return spotify_token
+
+    def query_song(self, song_name, artist):
+        spotify_token = self.__get_token()
+        song_name = "The Very First Night (Taylor's Version) (From The Vault)"
+        artist = "Taylor Swift"
+        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+                    song_name,
+                    artist
+                )
+        response = requests.get(query,headers={"Content-Type": "application/json","Authorization": "Bearer {}".format(spotify_token)})
+        response_json = response.json()
+        print(response_json)
+
+
+
+
+
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+spotify_token = auth_response_data['access_token']
+
+song_name = "The Very First Night (Taylor's Version) (From The Vault)"
+artist = "Taylor Swift"
+song_name = "wrap"
+artist = "Cardi B"
 query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
             song_name,
             artist
         )
 response = requests.get(query,headers={"Content-Type": "application/json","Authorization": "Bearer {}".format(spotify_token)})
 response_json = response.json()
-print(response_json)
+songs = response_json["tracks"]["items"]
+if songs == response_json:
+    print("response_json")
