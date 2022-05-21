beceb08da739c396c490484f0d89f6c13b8bf8e5
Aude Vernet <aude@Audes-MacBook-Air.local>
2022-05-21 14:51:17 +0100

    add unittest for atabaseManager class

--- TestDbManager.py
+++ TestDbManager.py
@@ -29,21 +29,39 @@ class TestDatabaseManager(TestCase):
         result = db._query_random_songs(nb_max_songs)
         self.assertEqual(expected, result)
         
-    @mock.patch('DatabaseManager.DatabaseManager._fetch_database', return_value=["song"])
-    def test_3_given_an_iterable_(self, song):
+    def test_given_an_iterable__manage_results_query_returns_a_list(self):
+        expected = ["a","b","c"]
+        results_iterable = [["","1","a"],["","2","b"],["","3","c"]]
+        connection = MagicMock()
+        db = DatabaseManager(connection)
+        result = db._manage_results_query(results_iterable)
+        self.assertEqual(expected, result)
+        
+    @mock.patch('DatabaseManager.DatabaseManager._manage_results_query', return_value=["song"])
+    def test_3_create_random_playlist(self, song):
         expected = ["song"]
         nb_max_songs = Mock()
         connection = MagicMock()
         db = DatabaseManager(connection)
-        result = db._query_random_songs(nb_max_songs)
+        result = db.create_random_playlist(nb_max_songs)
         self.assertEqual(expected, result)
     
-        def _manage_results_query(self, results):
-            list_of_songs = []
-        for result in results:
-            list_of_songs.append(result[2])
-        return list_of_songs
-    
-    
+    @mock.patch('DatabaseManager.DatabaseManager._manage_results_query', return_value=["song"])
+    def test_4_create_random_playlist(self, song):
+        nb_max_songs = Mock()
+        connection = MagicMock()
+        db = DatabaseManager(connection)
+        db.create_random_playlist(nb_max_songs)
+        db._manage_results_query.assert_called_once()
+     
+    @mock.patch('DatabaseManager.DatabaseManager._query_random_songs', return_value=["song"])
+    @mock.patch('DatabaseManager.DatabaseManager._manage_results_query', return_value=["song"])
+    def test_5_create_random_playlist(self, song, results):
+        nb_max_songs = Mock()
+        connection = MagicMock()
+        db = DatabaseManager(connection)
+        db.create_random_playlist(nb_max_songs)
+        db._query_random_songs.assert_called_once()
+
 if __name__ == '__main__':
     main()
